1b52746581a40214d8e3c70c92eb3724395b98cc
MrSzasz <lugoleandro@hotmail.com>
2022-07-13 15:31:02 -0300

    back

--- src/components/SkillsAndEd/SkillsAndEd.jsx
+++ src/components/SkillsAndEd/SkillsAndEd.jsx
@@ -14,8 +14,6 @@ import SkillBar from "../SkillBar/SkillBar";
 // ----------  COMPONENTS  ---------- //
 
 const SkillsAndEd = () => {
-
-  
   // ----------  RETURN  ---------- //
 
   return (
@@ -26,7 +24,7 @@ const SkillsAndEd = () => {
         </h2>
         <p>
           Desarrollo Front-End con base en ReactJS, diseño HTML y CSS puro,
-          ademas del uso de sus respectivos frameworks. Base de datos gracias al uso de Firebase.
+          ademas del uso de sus respectivos frameworks.
         </p>
       </div>
       <div className="skillsLevel">
@@ -43,8 +41,24 @@ const SkillsAndEd = () => {
         <SkillsBlock>
           <SkillBar name="GIT" value="70" />
           <SkillBar name="JQUERY" value="60" />
-          <SkillBar name="FIREBASE" value="50" />
         </SkillsBlock>
+      </div>
+
+      <div className="infoSkills">
+        <h2>
+          <CgDatabase /> Back-End
+        </h2>
+        <p>
+          Desarrollo Back-End con NodeJS para la utilización de JavaScript del lado del servidor. Manipulación de base de datos con FireBase.
+        </p>
+      </div>
+      <div className="skillsLevel">
+        <SkillsBlock>
+          <SkillBar name="FIREBASE" value="50" />
+          <SkillBar name="NODEJS" value="10" />
+        </SkillsBlock>
+        <SkillsBlock></SkillsBlock>
+        <SkillsBlock></SkillsBlock>
       </div>
     </section>
   );
